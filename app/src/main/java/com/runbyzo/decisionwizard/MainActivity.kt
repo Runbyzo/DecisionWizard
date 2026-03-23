a45cde30336f208b83de795f1e8a6e2a462190ff
Runbyzo <frunbyzo@gmail.com>
2026-03-23 10:54:29 +0300

    added other build files

--- app/src/main/java/com/runbyzo/decisionwizard/MainActivity.kt
+++ app/src/main/java/com/runbyzo/decisionwizard/MainActivity.kt
@@ -1,5 +1,4 @@
-package com.runbyzo.decisionwizard
-
+import androidx.compose.runtime.Composable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,10 +7,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.runbyzo.decisionwizard.decisionMethods.SAW
 import com.runbyzo.decisionwizard.ui.theme.DecisionWizardTheme
+
+
+val companies = listOf<String>("Mpstats.io", "Moneyplace.io", "Маяк", "MarketGuru", "Shopstat")
+
+val props = listOf(
+    doubleArrayOf(1.0,9.0,8.0,8.0,7.0),
+    doubleArrayOf(9.0,9.0,8.0,7.0,6.0),
+    doubleArrayOf(9.0,8.0,8.0,8.0,7.0),
+    doubleArrayOf(10.0,9.0,7.0,8.0,7.0),
+    doubleArrayOf(9.0,8.0,9.0,9.0,6.0),
+    doubleArrayOf(5.0,6.0,9.0,7.0,10.0)
+)
+
+val benefit = booleanArrayOf(true, true, true, true, false)
+val weights = doubleArrayOf(0.25, 0.25, 0.15, 0.25, 0.10)
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
